758175066fd5114d59f59c1719deb8a91bd8da22
take-tech1001 <tk.tech1001@gmail.com>
2021-10-05 14:24:27 +0900

    Formatted the code.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,18 @@
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
 interface Props {}
 class Document extends NextDocument<Props> {
   render() {
     return (
-      <Html lang="ja">
-        <Head>
-  
-        </Head>
+      <Html lang='ja'>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default Document
+export default Document;
